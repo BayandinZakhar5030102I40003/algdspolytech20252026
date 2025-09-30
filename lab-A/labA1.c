cae62aacf6d0cf7e669cea40859e0c0a93d7d942
BayandinZakhar5030102I40003 <bayandin.za@edu.spbstu.ru>
2025-09-30 22:47:16 +0300

    Проверка

---
+++ lab-A/labA1.c
@@ -0,0 +1,18 @@
+#pragma warning(disable:4996)
+
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRTDBG_MAP_ALLOC 
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <locale.h>
+
+
+int main(void) {
+	setlocale(LC_CTYPE, "Russian");
+	
+
+	return 0;
+}
+
